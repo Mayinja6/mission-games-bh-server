5d19e0da17e493ec0d50fd847667b73f581d6b06
dumani <dumanimedia@gmail.com>
2023-09-17 17:41:43 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+dotenv.config();
+
+import { notFound, errorHandler } from './utils/middleware.js';
+const PORT = process.env.PORT || 5000;
+const server = express();
+
+server.use(cookieParser());
+server.use(cors());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+server.use(notFound);
+server.use(errorHandler);
+
+server.listen(Number(PORT), () => console.log(`Server 🚀🚀 on port ${PORT}!`));
---
+++ src/utils/helpers.ts
@@ -0,0 +1,23 @@
+import { Response } from 'express';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+export const hashPassword = (password: string) => bcrypt.hash(password, 10);
+
+export const checkPassword = (inputPassword: string, hashedPassword: string) =>
+  bcrypt.compare(inputPassword, hashedPassword);
+
+export const generateToken = (res: Response, userInfo: { id: string }) => {
+  let token = jwt.sign({ ...userInfo }, process.env.JWT_SECRET!, {
+    expiresIn: '3d',
+  });
+  res.cookie('mission-games-bh', token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    sameSite: true,
+    maxAge: 3 * 24 * 60 * 60 * 1000,
+  });
+};
+
+export const verifyToken = (token: string) =>
+  jwt.verify(token, process.env.JWT_SECRET);
